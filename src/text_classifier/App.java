025862445175ba43f5ab6d281fa8c62d8852b556
Jasbir Singh <jasbircheema96@gmail.com>
2018-09-28 23:30:59 -0700

    File writer added

--- src/text_classifier/App.java
+++ src/text_classifier/App.java
@@ -2,6 +2,8 @@ package text_classifier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class App {
@@ -11,25 +13,28 @@ public class App {
 	static File testData=new File("resources/testdata.txt");
 	static File testLabels=new File("resources/testlabels.txt");
 	
-	
 	public static void main(String[] args) {	
 		try {
 			Trainer trainer=new Trainer(trainData,trainLabels);
 			trainer.train();
 			Classifier classifier=new Classifier(testData,testLabels,trainer);
 			List<String> predictedLabels=classifier.classify();
-			System.out.println("Classification accuracy - "+ classifier.getClassificationAccuracy());
-			//printLabels(predictedLabels);
+			writeToFile(predictedLabels,classifier);
 			
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void printLabels(List<String> predictedLabels) {
-		for(String label:predictedLabels) {
-			System.out.println(label);
+	public static void writeToFile(List<String> predictedLabels,Classifier classifier) throws IOException {
+		FileWriter fw=new FileWriter("resources/predictedLabels.txt");
+		for(String predictedLabel:predictedLabels) {
+			fw.write(predictedLabel+"\n");
 		}
+		fw.write("Classification Accuracy   ----  "+classifier.getClassificationAccuracy()   );
+		fw.close();
 	}
 
 }
